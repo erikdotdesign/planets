0090a6be6e6d49bfc11b561ede471dd11aaafacb
Erik <erik@Mac.lan>
2025-08-23 16:40:27 -0400

    added more moons, converted planet thumbnails to images

---
+++ ui-src/thumbnails.ts
@@ -0,0 +1,29 @@
+import { PlanetViewer } from './three-planet';
+import { PLANETS } from './constants'; // an array of 16 PlanetOptions or { type, textures }
+
+export async function generatePlanetThumbnails() {
+  const canvas = document.createElement('canvas');
+  const viewer = new PlanetViewer(canvas, {
+    lightMode: "neutral",
+    environment: false,
+    controls: false,
+    headless: true,
+    width: 64,
+    height: 64,
+  });
+
+  const previews: { name: string; image: string }[] = [];
+
+  for (const [name, planet] of Object.entries(PLANETS)) {
+    await viewer.setPlanet({
+      type: planet.type,
+      textures: planet.textures,
+      radius: planet.radius,
+    });
+
+    const image = await viewer.renderOnce();
+    previews.push({ name, image });
+  }
+
+  return previews;
+}
